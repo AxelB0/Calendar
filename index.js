462646bcf6efd4da34af957957356e79a43622cd
AxelBO <axel.boonen.mail@gmail.com>
2025-05-08 14:05:27 +0200

    logic

--- index.js
+++ index.js
@@ -2,13 +2,16 @@ console.log('Happy developing ✨')
 
 const months = ["JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"];
 let currentDay = 8;
-let firstDayName = 4;
-let currentMonth = 5;
+let firstDayName = 3;
+let currentMonth = 4;
 let currentYear = 2025;
 
 
 function fillInTheDays() {
-    for (let i = 1; i <= monthLengthChecker(currentMonth); i++) {
+    for (let i = 0; i <= monthLengthChecker(currentMonth); i++) {
+        let dayName = "day"
+        dayName += i;
+        document.getElementById()
 
 
 
@@ -17,16 +20,14 @@ function fillInTheDays() {
 }
 
 function monthLengthChecker(month) {
-    if (month === 2) {
+    if (month === 1) {
         return 28;
     }
-    if (month === 1|| month === 3 || month === 5 || month === 7 || month === 8 || month === 10 || month === 12) {
+    if ([0, 2, 4, 6, 7, 9, 11].includes(month)) {
         return 31;
-    }
-    if (month === 4 || month === 6 || month === 9 || month === 11) {
+    } else {
         return 30;
     }
-
 
 
 }
